6433336937289d31e44fca04c397e883d1563c40
cwl1997 <928024702@qq.com>
2021-01-25 23:22:26 +0800

    学习进度更新61

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -2,14 +2,35 @@ import React, { Component } from 'react'
 import './index.css'
 
 export default class Item extends Component {
+    
+    state={mouse:false}//鼠标移入移出
+
+    // 鼠标移入移出的回调
+    handleMouse = (flag)=>{
+        return ()=>{
+            // console.log(flag)
+            this.setState({mouse:flag})
+        }
+    }
+
+    // 勾选,取消的回调
+    handleCheck = (id)=>{
+        return (event)=>{
+            // console.log(id,event.target.checked)
+            this.props.updateTodo(id,event.target.checked)
+        }
+    }
+
     render() {
+        const {id, name ,done} = this.props
+        const {mouse} = this.state
         return (
-            <li>
+            <li style={{backgroundColor:mouse ? '#ddd':'white'}} onMouseLeave={this.handleMouse(false)} onMouseEnter={this.handleMouse(true)}>
                 <label>
-                    <input type="text"/>
-                    <span>xxx</span>
+                    <input type="checkbox" onChange={this.handleCheck(id)} defaultChecked={done}/>
+                    <span>{name}</span>
                 </label>
-                <button className="btn btn-danger" style={{display:'none'}}>删除</button>
+                <button className="btn btn-danger" style={{display:mouse?'block':'none'}}>删除</button>
             </li>
         )
     }
