d27ceae7bf20dcbbb9e5fcc51f6f1c1063a21cac
cwl1997 <928024702@qq.com>
2021-02-03 17:31:31 +0800

    进度更新

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import React, { Component } from 'react'
-import Search from './components/Search'
-import List from './components/List'
-// import './app.css'
 
 export default class App extends Component {
     render() {
         return (
             <div >
-               <Search/>
-               <List/>
+              App...
             </div>
-        )
+        )   
     }
 }
